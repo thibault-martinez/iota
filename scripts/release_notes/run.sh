ea236d50fb741c78d55b5f0a9956d73629845e2a
Chloe Martin <chloedaughterofmars@gmail.com>
2025-03-18 08:05:17 -0400

    Update run script

--- scripts/release_notes/run.sh
+++ scripts/release_notes/run.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-# Check if either "python" or "python3" exists and use it
-if command -v python3 &>/dev/null; then
-    PYTHON_CMD="python3"
-elif command -v python &>/dev/null; then
-    PYTHON_CMD="python"
-else
-    echo "Neither python nor python3 binary is installed. Please install Python."
-    exit 1
-fi
+source ../utils/python_venv_wrapper.sh
 
 $PYTHON_CMD release_notes.py "$@"
